5899d1063c996fa38d73ac1e9095e8e2e7ad711e
Marc CARRE <carre.marc@gmail.com>
2013-08-14 07:34:37 +0100

    Binomial trees' exercises (+ unit tests).

---
+++ src/main/java/com/carmatech/algo/datastructures/BinomialTree.java
@@ -0,0 +1,132 @@
+package com.carmatech.algo.datastructures;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinomialTree<T extends Comparable<T>> {
+	private final T[] tree;
+	private final int depth;
+	private final int numberOfPaths;
+
+	/**
+	 * @param tree
+	 *            array defining the binomial tree for the top-left (root) to the bottom-right element.
+	 */
+	public BinomialTree(final T[] tree) {
+		isValidTree(tree);
+		this.tree = tree;
+		this.depth = calculateDepth();
+		this.numberOfPaths = calculateNumberOfPaths();
+	}
+
+	private void isValidTree(final T[] tree) {
+		long sum = 0L;
+		long i = 1L;
+		while (sum < tree.length)
+			sum += i++;
+
+		if (sum != tree.length) {
+			final String cause = "Expected size for a binomial tree: [" + sum + "]. Actual size: [" + tree.length + "].";
+			throw new IllegalArgumentException("Provided array doesn't define a binomial tree: " + cause);
+		}
+	}
+
+	private int calculateDepth() {
+		return (int) (-1 + Math.sqrt(1 + (8 * tree.length))) / 2;
+	}
+
+	private int calculateNumberOfPaths() {
+		return (int) Math.pow(2, depth - 1);
+	}
+
+	public int depth() {
+		return depth;
+	}
+
+	public int numberOfPaths() {
+		return numberOfPaths;
+	}
+
+	/**
+	 * Generates all the possible paths in the binomial tree.
+	 * 
+	 * @return indexes in the provided array corresponding to the paths. 1st dimension = number of paths. 2nd dimension = depth of the tree/path.
+	 */
+	public int[][] paths() {
+		final List<int[]> allPaths = new LinkedList<int[]>();
+
+		visit(allPaths, initializePath(), 1, 0, 1);
+		visit(allPaths, initializePath(), 1, 0, 2);
+
+		return allPaths.toArray(new int[allPaths.size()][depth]);
+	}
+
+	private int[] initializePath() {
+		final int[] path = new int[depth];
+		path[0] = 0;
+		return path;
+	}
+
+	private void visit(final List<int[]> allPaths, final int[] path, final int currentDepth, final int parent, final int child) {
+		path[currentDepth] = child;
+
+		final int leftChild = child + currentDepth + 1;
+		final int rightChild = child + currentDepth + 2;
+
+		if (leftChild < tree.length) {
+			final int nextDepth = currentDepth + 1;
+			visit(allPaths, path, nextDepth, child, leftChild);
+			visit(allPaths, copy(path), nextDepth, child, rightChild);
+		} else {
+			allPaths.add(path);
+		}
+	}
+
+	private int[] copy(final int[] path) {
+		final int[] copy = new int[path.length];
+		System.arraycopy(path, 0, copy, 0, path.length);
+		return copy;
+	}
+
+	public int[] largestPath() {
+		final int[][] paths = paths();
+
+		long max = Long.MIN_VALUE;
+		int indexMax = -1;
+
+		for (int i = 0; i < numberOfPaths; i++) {
+			final long sum = sum(paths, i);
+			if (sum > max) {
+				max = sum;
+				indexMax = i;
+			}
+		}
+
+		return paths[indexMax];
+	}
+
+	public long largestPathValue() {
+		final int[][] paths = paths();
+
+		long max = Long.MIN_VALUE;
+
+		for (int i = 0; i < numberOfPaths; i++) {
+			final long sum = sum(paths, i);
+			if (sum > max) {
+				max = sum;
+			}
+		}
+
+		return max;
+	}
+
+	private long sum(final int[][] paths, final int i) {
+		long sum = 0L;
+		for (int j = 0; j < depth; j++) {
+			final int index = paths[i][j];
+			// TODO: check if T is "summable" before running largestPath and largestPathValue, to avoid the below nasty cast:
+			sum += (Integer) tree[index];
+		}
+		return sum;
+	}
+}
---
+++ src/test/java/com/carmatech/algo/datastructures/BinomialTreeTest.java
@@ -0,0 +1,77 @@
+package com.carmatech.algo.datastructures;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class BinomialTreeTest {
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
+	@Test
+	public void createValidBinomialTrees() {
+		BinomialTree<Integer> tree1 = new BinomialTree<Integer>(new Integer[] { 1 });
+		assertThat(tree1.depth(), is(1));
+		assertThat(tree1.numberOfPaths(), is(1));
+
+		BinomialTree<Integer> tree2 = new BinomialTree<Integer>(new Integer[] { 1, 2, 3 });
+		assertThat(tree2.depth(), is(2));
+		assertThat(tree2.numberOfPaths(), is(2));
+
+		BinomialTree<Integer> tree3 = new BinomialTree<Integer>(new Integer[] { 1, 2, 3, 4, 5, 6 });
+		assertThat(tree3.depth(), is(3));
+		assertThat(tree3.numberOfPaths(), is(4));
+
+		BinomialTree<Integer> tree4 = new BinomialTree<Integer>(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+		assertThat(tree4.depth(), is(4));
+		assertThat(tree4.numberOfPaths(), is(8));
+
+		BinomialTree<Integer> tree5 = new BinomialTree<Integer>(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
+		assertThat(tree5.depth(), is(5));
+		assertThat(tree5.numberOfPaths(), is(16));
+	}
+
+	@Test
+	public void createInvalidBinomialTrees() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage(is("Provided array doesn't define a binomial tree: Expected size for a binomial tree: [6]. Actual size: [5]."));
+		new BinomialTree<Integer>(new Integer[] { 1, 2, 3, 4, 5 });
+	}
+
+	@Test
+	public void generatePaths() {
+		BinomialTree<Integer> tree = new BinomialTree<Integer>(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+		int[][] paths = tree.paths();
+
+		printPaths(paths);
+
+		assertThat(paths.length, is(tree.numberOfPaths()));
+		assertThat(paths, equalTo(new int[][] { new int[] { 0, 1, 3, 6 }, new int[] { 0, 1, 3, 7 }, new int[] { 0, 1, 4, 7 }, new int[] { 0, 1, 4, 8 },
+				new int[] { 0, 2, 4, 7 }, new int[] { 0, 2, 4, 8 }, new int[] { 0, 2, 5, 8 }, new int[] { 0, 2, 5, 9 } }));
+	}
+
+	private void printPaths(int[][] paths) {
+		for (int i = 0; i < paths.length; i++) {
+			for (int j = 0; j < paths[0].length; j++) {
+				System.out.print(paths[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
+	}
+
+	@Test
+	public void findLargestPath() {
+		BinomialTree<Integer> tree = new BinomialTree<Integer>(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+		assertThat(tree.largestPath(), equalTo(new int[] { 0, 2, 5, 9 }));
+	}
+
+	@Test
+	public void findLargestPathValue() {
+		BinomialTree<Integer> tree = new BinomialTree<Integer>(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+		assertThat(tree.largestPathValue(), is(Long.valueOf(1 + 3 + 6 + 10)));
+	}
+}
