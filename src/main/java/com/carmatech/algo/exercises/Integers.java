b93f66cab65a98fb91f52f56b8ee5b5097e0e92c
Marc CARRE <carre.marc@gmail.com>
2015-03-01 16:06:33 +0000

    Find two integers summing to target: sorting and hash map.

--- src/main/java/com/carmatech/algo/exercises/Integers.java
+++ src/main/java/com/carmatech/algo/exercises/Integers.java
@@ -1,5 +1,9 @@
 package com.carmatech.algo.exercises;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Integers {
     public static final int[] NO_SOLUTION = new int[0];
 
@@ -15,6 +19,53 @@ public class Integers {
             for (int j = i + 1; j < array.length; ++j)
                 if (array[i] + array[j] == target)
                     return new int[] {i, j};
+
+        return NO_SOLUTION;
+    }
+
+    /**
+     * Time complexity:  O(n.(log(n)+1)) = O(n.log(n))
+     * Space complexity: O(1)
+     */
+    public static int[] findTwoIntegersSummingUpToTarget_SortingAndSimplePass(final int target, final int[] array) {
+        if ((target == 0) || (array == null) || (array.length < 2))
+            return NO_SOLUTION;
+
+        Arrays.sort(array);
+        int i = 0;
+        int j = array.length - 1;
+        int sum;
+        while (i < j) {
+            sum = array[i] + array[j];
+            if (sum == target) return new int[] {i, j};
+            if (sum < target) ++i;
+            if (sum > target) --j;
+        }
+        return NO_SOLUTION;
+    }
+
+    public static int[] findTwoIntegersSummingUpToTarget_HashMap(final int target, final int[] array) {
+        if ((target == 0) || (array == null) || (array.length < 2))
+            return NO_SOLUTION;
+
+        int remainder;
+        int current;
+        final Map<Integer, Integer> remainders = new HashMap<Integer, Integer>();
+        for (int i = 0; i < array.length; ++i) {
+            current = array[i];
+            if (remainders.containsKey(current))
+                return new int[] {remainders.get(current), i};
+
+            remainder = target - current;
+            if (!remainders.containsKey(remainder))
+                remainders.put(remainder, i);
+        }
+
+        for (int i = 0; i < array.length; ++i) {
+            current = array[i];
+            if (remainders.containsKey(current))
+                return new int[]{remainders.get(current), i};
+        }
 
         return NO_SOLUTION;
     }
--- src/test/java/com/carmatech/algo/exercises/IntegersTest.java
+++ src/test/java/com/carmatech/algo/exercises/IntegersTest.java
@@ -19,4 +19,26 @@ public class IntegersTest {
         assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_BruteForce(12, new int[]{1, 2, 2, 2, 5, 5, 5, 8}), NO_SOLUTION), is(true));
         assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_BruteForce(13, new int[]{1, 2, 2, 2, 5, 5, 5, 8}), new int[] {4, 7}), is(true));
     }
+
+    @Test
+    public void findTwoIntegersSummingUpToTarget_SortingAndSimplePass() {
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_SortingAndSimplePass(0, new int[]{1, 2, 3}), NO_SOLUTION), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_SortingAndSimplePass(5, new int[0]), NO_SOLUTION), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_SortingAndSimplePass(5, new int[]{1}), NO_SOLUTION), is(true));
+
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_SortingAndSimplePass(5, new int[]{1, 2, 3}), new int[]{1, 2}), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_SortingAndSimplePass(12, new int[]{1, 2, 2, 2, 5, 5, 5, 8}), NO_SOLUTION), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_SortingAndSimplePass(13, new int[]{1, 2, 2, 2, 5, 5, 5, 8}), new int[] {4, 7}), is(true));
+    }
+
+    @Test
+    public void findTwoIntegersSummingUpToTarget_HashMap() {
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_HashMap(0, new int[]{1, 2, 3}), NO_SOLUTION), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_HashMap(5, new int[0]), NO_SOLUTION), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_HashMap(5, new int[]{1}), NO_SOLUTION), is(true));
+
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_HashMap(5, new int[]{1, 2, 3}), new int[]{1, 2}), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_HashMap(12, new int[]{1, 2, 2, 2, 5, 5, 5, 8}), NO_SOLUTION), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_HashMap(13, new int[]{1, 2, 2, 2, 5, 5, 5, 8}), new int[] {4, 7}), is(true));
+    }
 }
