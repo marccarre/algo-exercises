3d9e9610c7a2e96aca748fa5228241da888d51ec
Marc CARRE <carre.marc@gmail.com>
2015-03-01 15:50:03 +0000

    Find two integers summing to target: added tests.

--- src/main/java/com/carmatech/algo/exercises/Integers.java
+++ src/main/java/com/carmatech/algo/exercises/Integers.java
@@ -1,7 +1,7 @@
 package com.carmatech.algo.exercises;
 
 public class Integers {
-    public static final int[] NOT_FOUND = new int[0];
+    public static final int[] NO_SOLUTION = new int[0];
 
     /**
      * Time complexity:  O(n^2)
@@ -9,13 +9,13 @@ public class Integers {
      */
     public static int[] findTwoIntegersSummingUpToTarget_BruteForce(final int target, final int[] array) {
         if ((target == 0) || (array == null) || (array.length < 2))
-            return NOT_FOUND;
+            return NO_SOLUTION;
 
         for (int i = 0; i < array.length; ++i)
             for (int j = i + 1; j < array.length; ++j)
                 if (array[i] + array[j] == target)
                     return new int[] {i, j};
 
-        return NOT_FOUND;
+        return NO_SOLUTION;
     }
 }
--- src/test/java/com/carmatech/algo/exercises/IntegersTest.java
+++ src/test/java/com/carmatech/algo/exercises/IntegersTest.java
@@ -4,17 +4,19 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
+import static com.carmatech.algo.exercises.Integers.NO_SOLUTION;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class IntegersTest {
     @Test
     public void findTwoIntegersSummingUpToTarget_BruteForce() {
-        final int[] noSolution = new int[0];
-        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_BruteForce(0, new int[]{1, 2, 3}), noSolution), is(true));
-        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_BruteForce(5, new int[0]), noSolution), is(true));
-        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_BruteForce(5, new int[]{1}), noSolution), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_BruteForce(0, new int[]{1, 2, 3}), NO_SOLUTION), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_BruteForce(5, new int[0]), NO_SOLUTION), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_BruteForce(5, new int[]{1}), NO_SOLUTION), is(true));
 
         assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_BruteForce(5, new int[]{1, 2, 3}), new int[]{1, 2}), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_BruteForce(12, new int[]{1, 2, 2, 2, 5, 5, 5, 8}), NO_SOLUTION), is(true));
+        assertThat(Arrays.equals(Integers.findTwoIntegersSummingUpToTarget_BruteForce(13, new int[]{1, 2, 2, 2, 5, 5, 5, 8}), new int[] {4, 7}), is(true));
     }
 }
