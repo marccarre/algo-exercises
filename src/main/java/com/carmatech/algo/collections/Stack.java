dbbaccdd4b5e149bdc9b26df98a31a5b1791f6e3
Marc CARRE <carre.marc@gmail.com>
2015-03-11 06:51:51 +0000

    Added stack implementation using linked list (+tests).

---
+++ src/main/java/com/carmatech/algo/collections/Stack.java
@@ -0,0 +1,32 @@
+package com.carmatech.algo.collections;
+
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+    private Node<T> top = null;
+
+    private class Node<T> {
+        private final Node<T> next;
+        private final T value;
+        Node(final Node<T> next, final T value) {
+            this.next = next;
+            this.value = value;
+        }
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void push(final T value) {
+        top = new Node(top, value);
+    }
+
+    public T pop() {
+        if (isEmpty())
+            throw new EmptyStackException();
+        final T value = top.value;
+        top = top.next;
+        return value;
+    }
+}
