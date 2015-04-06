0ee249d071478d3d8947050edca324390b6ac6b6
Marc CARRE <carre.marc@gmail.com>
2015-04-06 11:19:53 +0100

    Added hard test case for sudoku backtracking DFS.

--- src/test/java/com/carmatech/algo/backtracking/SudokuTest.java
+++ src/test/java/com/carmatech/algo/backtracking/SudokuTest.java
@@ -103,7 +103,7 @@ public class SudokuTest {
     }
 
     @Test
-    public void resolve() {
+    public void resolveMediumDifficultySudoku() {
         sudoku.resolve();
         assertThat(sudoku.isSolutionFound(), is(true));
         assertThat(sudoku.grid(), is(new int[][] {
@@ -118,6 +118,40 @@ public class SudokuTest {
                 { 5, 1, 9,   3, 2, 6,   8, 7, 4 },
                 { 2, 4, 8,   9, 5, 7,   1, 3, 6 },
                 { 7, 6, 3,   4, 1, 8,   2, 5, 9 }
+        }));
+    }
+
+    @Test
+    public void resolveHardDifficultySudoku() {
+        int[][] grid = new int[][] {
+                { 0, 0, 0,   0, 0, 0,   0, 1, 2 },
+                { 0, 0, 0,   0, 3, 5,   0, 0, 0 },
+                { 0, 0, 0,   6, 0, 0,   0, 7, 0 },
+
+                { 7, 0, 0,   0, 0, 0,   3, 0, 0 },
+                { 0, 0, 0,   4, 0, 0,   8, 0, 0 },
+                { 1, 0, 0,   0, 0, 0,   0, 0, 0 },
+
+                { 0, 0, 0,   1, 2, 0,   0, 0, 0 },
+                { 0, 8, 0,   0, 0, 0,   0, 4, 0 },
+                { 0, 5, 0,   0, 0, 0,   6, 0, 0 }
+        };
+        Sudoku sudoku = new Sudoku(grid);
+        sudoku.resolve();
+
+        assertThat(sudoku.isSolutionFound(), is(true));
+        assertThat(sudoku.grid(), is(new int[][] {
+                { 6, 7, 3,   8, 9, 4,   5, 1, 2 },
+                { 9, 1, 2,   7, 3, 5,   4, 8, 6 },
+                { 8, 4, 5,   6, 1, 2,   9, 7, 3 },
+
+                { 7, 9, 8,   2, 6, 1,   3, 5, 4 },
+                { 5, 2, 6,   4, 7, 3,   8, 9, 1 },
+                { 1, 3, 4,   5, 8, 9,   2, 6, 7 },
+
+                { 4, 6, 9,   1, 2, 8,   7, 3, 5 },
+                { 2, 8, 7,   3, 5, 6,   1, 4, 9 },
+                { 3, 5, 1,   9, 4, 7,   6, 2, 8 }
         }));
     }
 }
